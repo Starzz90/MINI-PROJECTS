e8dacc8f2e2da775d3f460e83ab8318d04612726
Starzz90 <enzodtjen@gmail.com>
2026-03-22 00:19:01 -0500

    not done updating

---
+++ WEB-CALC/CALC.js
@@ -0,0 +1,62 @@
+let display = document.getElementById('display')
+
+function getNumber(num){
+    display.value += num;
+}
+
+function clearDisplay(){
+    display.value = '';
+}
+
+//operator basic//
+
+function checkOperator(op){
+    if(display.value == ''){
+        return;
+    }
+    
+    const char = display.value.slice(-1);
+    if('+-*/'.includes(char)) return;
+    display.value += op;
+}
+
+function sqrt(op){
+    if(display.value == ''){
+        return;
+}
+    const char = display.value.slice(-1);
+    if('+-*/'.includes(char)) return;
+    display.value = Math.SQRT(Number(display.value), 2);
+}
+
+function customsqrt(op){
+    if(display.value === ''){
+        return;
+    }
+    const char = display.value.slice(-1);
+    if('+-/*'.includes(char))return;
+    display.value += op;
+}
+
+function power(op){
+    if(display.value === ''){
+        return;
+    }
+    const char = display.value.slice(-1);
+    if('+-*/'.includes(char)) return;
+    display.value += op;
+
+}
+
+function results(){
+    try{
+        var result = eval(display.value);
+        display.value = result
+        
+    }catch{
+        display.value = "Error"; 
+    }
+}
+function phi(op){
+    display.value = Math.PI;
+}
