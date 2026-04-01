3767eaf37def4d2375b487758e8609ed4f93e8a5
Starzz90 <enzodtjen@gmail.com>
2026-04-01 09:28:51 -0500

    Adding PHP

---
+++ PHP PRACTICE/second.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample HTML Page</title>
+    <link rel="stylesheet" href="second.css">
+    
+</head>
+<body>
+    <div class="container">
+    <div class="input-group">
+        <form action="result.php" method="FORM">
+            <h1>Profile Form</h1>
+            <label>Name</label></br>
+            <input class="inputbox" type="text" name = "name" placeholder="Name here..."></br>
+            <label >Age</label></br>
+            <input class= "inputbox" type="text" age = "age" placeholder="Age here..."></br>
+            <label >GPA</label></br>
+            <input class= "inputbox" type="text" GPA = "GPA" placeholder="GPA here..."></br>
+            <label >Country</label></br>
+            <input class= "inputbox" type="text" country = "Country" placeholder="Country here..."></br>
+            <button type= "Submit" class="buttonadd" >See your profile</button></br>
+        </form>
+        </div>
+    </div>
+</body>
+</html>
